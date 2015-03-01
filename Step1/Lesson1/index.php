141eb95d52dc7b693c26c71a409f2b92905762af
WiseMan CAx <wisemancax@gmail.com>
2015-03-01 17:49:50 +0200

    Lessons
    Lesson 0 and Lesson 1

---
+++ Step1/Lesson1/index.php
@@ -0,0 +1,29 @@
+<! DOCTYPE html>
+<html>
+
+<head>
+  <title>Lesson 1: My simple CMS with Basic skills</title>
+</head>
+
+<body>
+
+  <h3>Hi! Wellcome to lesson One.</h3>
+  <h3>Web site: vBox - http://vbox7.com/play:48ae13cac7</h3>
+  <h3>Web site: youtube - https://www.youtube.com/watch?v=rAPEQvsLWgk&feature=youtu.be</h3>
+  
+  <p>1. HTML 5 => Hypertext Markup Language 5</p>
+  <p>2. CSS2 => Cascading Style Sheets, Level 2</p>
+  <p>3. MySQL and MySQL(i)</p>
+  <p>4. JS => JavaScript, Ajax, jQuery</p>
+  <p>5. Understanding OOP and MVC concepts</p>
+  <p>6. PHP framework (from Scratch and CakePHP)</p>
+  <p>7. Good written and spoken English</p>
+  <p>8. and More...</p>
+
+</body>
+
+<footer>
+  <h3>Good bye and to see next meetings!</h3>
+</footer>
+
+</html>
