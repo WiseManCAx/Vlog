549c59baec27353ed2ff23b434d2f0a80d87641e
WiseMan CAx <wisemancax@gmail.com>
2015-03-03 20:59:08 +0200

    step2
    Create a template for step2

---
+++ step2/lesson2/index-template-end.php
@@ -0,0 +1,68 @@
+<! DOCTYPE html>
+<html>
+
+<!-- Header :: Begin -->
+
+<!-- WiseMan CAx to Text                                        
+       .   .   .             .   .                 ,--„ .   
+        \  |\  |             |\  |\                |    |\  
+         \ | \ | . .--. .-.  | \ | \   .--. .--.   |    | \ 
+          \|  \| | `--.(.-'  |  \|  \  |  | |  |   |    |––\  \/
+           '   '-`-`--'-`--'-'   '   '-'--\-'  `---`--“-'   '-/\.
+-->
+
+<head>
+
+  <link href="./favicon.ico" rel="shortcut icon" type="image/x-icon" />
+	
+  <!-- CSS :: Begin -->
+  <link href="./css/reset.css" rel="stylesheet" type="text/css" />
+  <link href="./css/bootstrap.css" rel="stylesheet" type="text/css" />
+  <link href="./css/css.css" rel="stylesheet" type="text/css" />
+  <!-- CSS :: End -->
+  
+  <!-- JavaScript :: Begin -->
+  <script type="text/JavaScript" language="javascript" src="./js/menu.js"></script>
+  <script type="text/javascript" language="javascript" src="./js/jquery.js"></script>
+  <!-- JavaScript :: End -->
+
+  <!-- MetaTags :: Begin -->
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta http-equiv="content-language" content="bg" />
+  <meta name="copyright" content="http://www.wisemancax.com/" />
+  <meta name="description" content="Tutorials for PHP :: WiseMan CAx™ - Computer Aided eXchange (All purpose" />
+  <meta name="keywords" content="cms, wisemancax, tutorial, php, mysql" />
+  <!-- MetaTags :: End -->
+  
+  <title>Lesson 2: My simple CMS with Basic skills</title>
+  
+</head>
+<!-- Header :: End -->
+
+<body>
+<!-- Wrapper :: Begin -->
+<!-- Wrapper :: End -->
+
+<!-- Body :: Begin -->
+  <h3>Hi! Wellcome to lesson Two.</h3>
+  <h3>Web site: vBox - http://vbox7.com/...</h3>
+  <h3>Web site: youtube - https://www.youtube.com/...</h3>
+  
+  <p>1. HTML 5 => Hypertext Markup Language 5</p>
+  <p>2. CSS2 => Cascading Style Sheets, Level 2</p>
+  <p>3. MySQL and MySQL(i)</p>
+  <p>4. JS => JavaScript, Ajax, jQuery</p>
+  <p>5. Understanding OOP and MVC concepts</p>
+  <p>6. PHP framework (from Scratch and CakePHP)</p>
+  <p>7. Good written and spoken English</p>
+  <p>8. and More...</p>
+<!-- Body :: End -->
+
+<!-- Footer :: Begin -->
+<div id="footer">
+  <h3>Good bye and to see next meetings!</h3>
+</div>
+<!-- Footer :: End -->
+
+</body>
+</html>
